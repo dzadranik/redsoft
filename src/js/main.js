633b54f6a4612dc8d6008eae98e23a93e611a826
Victoria <dzadranik@gmail.com>
2020-08-09 16:29:36 +0300

    add ie polyfill

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,12 @@
 import '../sass/main.scss'
 
+/*eslint-disable no-unused-vars*/
+import fetch from 'whatwg-fetch'
+import { polyfill } from 'es6-promise'
+import 'nodelist-foreach-polyfill'
+
+polyfill()
+
 document.addEventListener('DOMContentLoaded', function () {
     class Basket {
         constructor() {
@@ -10,38 +17,45 @@ document.addEventListener('DOMContentLoaded', function () {
                 '<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>'
         }
         initBasket() {
-            if (localStorage.getItem('basket') !== null) {
+            if (
+                localStorage.getItem('basket') !== '' &&
+                localStorage.getItem('basket') !== null
+            ) {
                 this.basketItems = localStorage.getItem('basket').split(',')
                 this.basketItems.map(item => {
-                    let element = document
-                        .querySelector(`.js-product[data-id='${item}']`)
+                    let element = document.querySelector(
+                        `.js-product[data-id='${item}']`
+                    )
                     element.classList.add('btn__in-basket')
                     element.innerHTML = this.btnInBasket
                 })
             }
         }
         setBasket(id, element) {
+            element.disabled = true
             element.innerHTML = this.btnLoader
-            fetch('https://reqres.in/api/produts/3', {
-                method: 'get',
-            })
+            window
+                .fetch('https://reqres.in/api/produts/3', {
+                    method: 'get',
+                })
                 .then(resp => resp.json())
                 .then(resp => {
                     console.log(resp)
                     element.classList.toggle('btn__in-basket')
 
-                    let indexBasket = this.basketItems.findIndex(
-                        item => item === id
-                    )
+                    let indexBasket = this.basketItems.indexOf(id)
                     if (indexBasket === -1) {
                         this.basketItems.push(id)
-                    } else this.basketItems.splice(indexBasket, 1)
-                    localStorage.setItem('basket', this.basketItems.join())
+                    } else {
+                        this.basketItems.splice(indexBasket, 1)
+                    }
+                    localStorage.setItem('basket', this.basketItems.join(','))
                 })
                 .catch(function (err) {
                     console.log('Fetch Error :-S', err)
                 })
                 .finally(() => {
+                    element.disabled = false
                     if (element.classList.contains('btn__in-basket')) {
                         element.innerHTML = this.btnInBasket
                     } else {
@@ -54,12 +68,14 @@ document.addEventListener('DOMContentLoaded', function () {
     basket.initBasket()
 
     const btnBuy = document.querySelectorAll('.js-product')
-    btnBuy.forEach(item => {
-        item.addEventListener('click', function (event) {
-            let productId = event.currentTarget.dataset.id
-            basket.setBasket(productId, event.currentTarget)
+    if (btnBuy.length > 0) {
+        btnBuy.forEach(item => {
+            item.addEventListener('click', function (event) {
+                let productId = event.currentTarget.dataset.id
+                basket.setBasket(productId, event.currentTarget)
+            })
         })
-    })
+    }
 
     //search
     document.getElementById('search').addEventListener('input', event => {
